a59b3bfdf28deb33c0f1dd158db1e853c00e1d8a
alberto801263 <68609211+alberto801263@users.noreply.github.com>
2020-07-21 19:24:16 -0400

    intitialiser_point

--- Travail_pratique_2/Travail_Pratique_2_etudiant/geometrie/t_point2d.cpp
+++ Travail_pratique_2/Travail_Pratique_2_etudiant/geometrie/t_point2d.cpp
@@ -1,3 +1,38 @@
-//alllo
-//ALLO
-//ALLOasd asd asda sd
+/*****************************************************************************
+	Par        :	David Beaulieu et Hugues Saulnier
+	Revise le  :	juin 2020
+	Fichier    :	R3.c
+	Objectifs  :	Librairie qui permet de modéliser un point dans l'espace
+	Note	   :	NA
+*****************************************************************************/
+
+/****************************************************************************/
+/*							COMMANDES PREPROCESSEUR							*/
+/****************************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+#include"t_point2d.h"
+
+/*
+	NOM	:			nouveauPoint
+	DEF :			Fonction constructeur qui permet de créer un nouveau point
+					Selon les valeurs donnée en paramètre
+	PARAMETRES :	Aucun
+	RETOUR :		l'adresse du point créer
+	SPÉCIFICATION :
+*/
+Premier_point nouveauPoint(double x, double y) {
+
+	ptr_premier point;
+
+	// Allocation mémoire de la structure.
+	point = (ptr_R3)malloc(sizeof(struct t_R3));
+
+	point->x = x;
+	point->y = y;
+	
+
+	return point;
--- Travail_pratique_2/Travail_Pratique_2_etudiant/geometrie/t_point2d.h
+++ Travail_pratique_2/Travail_Pratique_2_etudiant/geometrie/t_point2d.h
@@ -1,2 +1,23 @@
-//test
-//TEST2
+/*****************************************************************************
+	Par        :	
+	Revise le  :	juin 2020
+	Fichier    :	R3.h
+	Objectifs  :	La création d'un type bien réel : les vecteurs et les points de l'espace
+					ordinaire qu'on appelle traditionnellement R3
+	Note	   :	UN POINT EST ASSIMILÉ À SON VECTEUR POSITION
+					et c'est le type de base à toute implémentation
+					de la géométrie élémentaire
+*****************************************************************************/
+
+/****************************************************************************/
+/*							COMMANDES PREPROCESSEUR							*/
+/****************************************************************************/
+#ifndef __MODULE_POINTS_R3__
+#define __MODULE_POINTS_R3__ 
+
+
+/****************************************************************************/
+/*							DEFINITIONS DES TYPES							*/
+/****************************************************************************/
+// Pointeur d'un ptr_R3 - pointeur vers un point dans l'espace.
+typedef struct Premier_point* ptr_premier;
